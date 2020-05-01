aaf1589ce7dab7730660f3f3f356e633122802f1
Amir Shariff <amirshariff@msn.com>
2020-05-01 15:52:14 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Route } from 'react-router-dom'
+import Landing from './components/Landing'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PicCard from './components/PicCard';
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Nav from './components/Nav';
+import NavigationBar from './utility/NavigationBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  
+    
+    state = {
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: ''
+    };
+  
+  componentDidMount() {
+    if (this.state.logged_in) {
+      fetch('http://localhost:8000/core/current_user/', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({ username: json.username });
+        });
+    }
+  }
+
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch('http://127.0.0.1:8000/api/auth/token/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          // username: json.user
+        });
+      });
+  };
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/core/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true
+        });
+      });
+  };
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+  
+  render() {
+
+  
+    return (
+      <div >
+        <NavigationBar
+          logged_in={this.state.logged_in}
+          display_form={this.display_form}
+          handle_logout={this.handle_logout}
+          />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp}/>
+          <Route exact path='/home' component={PicCard} />
+        <h3>
+          {this.state.logged_in
+            ? `Hello, ${this.state.username}`
+            : 'Please Log In'}
+        </h3>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/utility/NavigationBar.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom'
+
+
+
+const NavigationBar = (props) => {
+    return (
+			<div>
+				<Navbar bg="dark" variant="light">
+					<Navbar.Brand ><NavLink to='/'>Solito</NavLink></Navbar.Brand>
+				<Nav className="mr-auto">
+
+					{!props.logged_in ?
+						<Fragment>
+							<NavLink to='login'  > Login</NavLink>
+							<NavLink to='signup' > Sign Up</NavLink> 
+						</Fragment>
+						:
+						<NavLink to='logout' onClick={props.handle_logout}> Log Out</NavLink>
+						
+					}
+
+
+					</Nav>
+					
+				</Navbar>
+			</div>
+		);
+}
+export default NavigationBar;
